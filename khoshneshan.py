8d5d367ef2c0c8eda73c821c28bf8961cda41b69
arsham ganjapour <ggod818181@gmail.com>
2024-05-30 02:22:26 +0330

    khoshneshan.py added.

---
+++ khoshneshan.py
@@ -0,0 +1,15 @@
+from requests import api
+from json import dumps, dump, loads
+from fake_useragent import FakeUserAgent
+
+
+user_agent = FakeUserAgent()
+response = api.get(
+    url=r'https://api.khoshneshan.com/api/loads',
+    headers={'User-Agent': user_agent.random}
+)
+
+with open('khoshneshan-loads.json', 'wt', encoding='utf-8') as file:
+    file.write(dumps(response.json(), indent=4))
+    file.close()
+    
